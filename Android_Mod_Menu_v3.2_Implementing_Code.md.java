1e58de66b6af0621934a57ddf09255abb92deb11
tusar khan <114900895+MrTusarRX@users.noreply.github.com>
2023-03-09 16:24:51 +0530

    Add files via upload

---
+++ Android_Mod_Menu_v3.2_Implementing_Code.md.java
@@ -0,0 +1,12 @@
+//Overlay Permission (Add this under any uses-permission in Android manifest 
+
+<uses-permission android:name="android.permission.SYSTEM_ALERT_WINDOW"/>
+
+//Service (Add this above application tag in Android Manifest)
+
+<service android:name="com.android.support.Launcher" android:enabled="true"
+    android:exported="false" android:stopWithTask="true" />
+    
+//Add this in MainActivity of Game below onCreate Method
+
+invoke-static {p0}, Lcom/android/support/Main;->Start(Landroid/content/Context;)V
